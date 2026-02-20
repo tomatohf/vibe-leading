6e0b804bb17ad1ae1d3d4aacb1208fe34adaacc8
HF <Tomato.HF@gmail.com>
2026-02-20 12:41:12 +0800

    prepare to connect ui to agent via ag-ui

--- agent/src/agent/server.py
+++ agent/src/agent/server.py
@@ -1,12 +1,58 @@
 #!/usr/bin/env python
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+import asyncio
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
 import uvicorn
+
+from ag_ui.core import (
+    RunAgentInput,
+    RunFinishedEvent,
+    RunErrorEvent,
+)
+from ag_ui.encoder import EventEncoder
 
 load_dotenv()
 
 app = FastAPI()
+
+@app.post('/')
+async def agui_endpoint(client_input: RunAgentInput, request: Request):
+    """AG-UI protocol endpoint"""
+
+    # Get the accept header from the request
+    accept_header = request.headers.get("accept")
+    # Create an event encoder to properly format SSE events
+    agui_encoder = EventEncoder(accept=accept_header)
+
+    thread_id = client_input.thread_id
+    run_id = client_input.run_id
+    agent_inputs = {
+        **client_input.state,
+        'messages': [message.model_dump() for message in client_input.messages],
+        'copilotkit': {
+            'actions': [
+                {
+                    'type': 'function',
+                    'function': tool.model_dump(),
+                }
+                for tool in client_input.tools
+            ]
+        },
+        'id': thread_id,
+    }
+
+    async def event_generator():
+        try:
+            # FIXME: TBD
+            yield agui_encoder.encode(RunFinishedEvent(thread_id=thread_id, run_id=run_id))
+
+        except Exception as e:
+            yield agui_encoder.encode(RunErrorEvent(message=str(e)))
+
+    return StreamingResponse(event_generator(), media_type=agui_encoder.get_content_type())
+
 
 def start_server():
     """Run the uvicorn server."""
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,6 +2,7 @@ import { CopilotChat } from "@copilotkit/react-ui";
 
 export default function Chat() {
   return (
+    // TODO: 使用实际的 agent 相关数据替换以下信息
     <CopilotChat
       instructions={"You are assisting the user as best as you can. Answer in the best way possible given the data you have."}
       labels={{
